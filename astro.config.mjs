dd7ef6829cc7279ee48d692d54e6cbb719106efa
Benjaminamos11 <benjaminamoswagner@gmail.com>
2025-05-14 19:57:25 +0800

    Fix Astro configuration and dependency issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import alpinejs from '@astrojs/alpinejs';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://relofinder.ch',
   integrations: [
     tailwind(),
     react(),
     alpinejs()
-  ],
-  vite: {
-    ssr: {
-      noExternal: ['@astrojs/*']
-    }
-  }
+  ]
 });
