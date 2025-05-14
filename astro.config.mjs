d2cac72a6cd4d00609f6e68479ddfd714ed2155e
Benjaminamos11 <benjaminamoswagner@gmail.com>
2025-05-14 20:24:23 +0800

    Fix Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,22 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import alpinejs from '@astrojs/alpinejs';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://relofinder.ch',
   integrations: [
-    tailwind(), 
-    react(), 
+    tailwind(),
+    react(),
     alpinejs()
   ],
   vite: {
+    build: {
+      sourcemap: true
+    },
     optimizeDeps: {
-      exclude: ['@supabase/supabase-js', '@rollup/plugin-node-resolve']
+      exclude: ['@supabase/supabase-js']
     },
     ssr: {
-      noExternal: ['@supabase/supabase-js', '@rollup/plugin-node-resolve']
+      noExternal: ['@supabase/supabase-js']
     }
   }
 });
